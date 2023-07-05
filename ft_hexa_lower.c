c421b6b4b63357456225d395b41bac0c6b12eff8
Francisco Javier Costa Ferrer-Vidal <fcosta-f@car11s1.42barcelona.com>
2023-07-05 12:44:41 +0200

    lower and upper hexa fixed to protect writes

--- ft_hexa_lower.c
+++ ft_hexa_lower.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_hexa_lower.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fcosta-f <fcosta-f@student.42barcelona.    +#+  +:+       +#+        */
+/*   By: fcosta-f <fcosta-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 21:19:39 by fcosta-f          #+#    #+#             */
-/*   Updated: 2023/07/05 12:35:58 by fcosta-f         ###   ########.fr       */
+/*   Updated: 2023/07/05 12:43:24 by fcosta-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,16 @@ int	ft_i_hexa_lower(int n, int *counter)
 			return (-1);
 	}
 	if ((n % 16) < 10)
-	{	
+	{
 		if (ft_putchar((n % 16) + '0') == -1)
 			return (-1);
-		*counter = *counter  + 1;
+		*counter = *counter + 1;
 	}
 	else
 	{
 		if (ft_putchar((n % 16) - 10 + 'a') == -1)
 			return (-1);
-		*counter = *counter  + 1;
+		*counter = *counter + 1;
 	}
 	return (*counter);
 }
@@ -39,10 +39,10 @@ int	ft_hexa_lower(int n, int *counter)
 	return (ft_i_hexa_lower(n, counter));
 }
 
-int main()
+/*int main()
 {
     int counter = 0;
     printf("%d", ft_hexa_lower(255, &counter));
 
     return 0;
-}
+}*/
--- ft_hexa_upper.c
+++ ft_hexa_upper.c
@@ -3,25 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   ft_hexa_upper.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fcosta-f <fcosta-f@student.42barcelona.    +#+  +:+       +#+        */
+/*   By: fcosta-f <fcosta-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 20:46:57 by fcosta-f          #+#    #+#             */
-/*   Updated: 2023/07/04 21:18:55 by fcosta-f         ###   ########.fr       */
+/*   Updated: 2023/07/05 12:43:46 by fcosta-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	ft_hexa_upper(int n)
+int	ft_i_hexa_upper(int n, int *counter)
 {
-	int	counter;
-
-	counter = 0;
 	if (n > 15)
-		counter += ft_hexa_upper(n / 16);
+	{
+		if (ft_i_hexa_upper(n / 16, counter) == -1)
+			return (-1);
+	}
 	if ((n % 16) < 10)
-		ft_putchar((n % 16) + '0');
+	{
+		if (ft_putchar((n % 16) + '0') == -1)
+			return (-1);
+		*counter = *counter + 1;
+	}
 	else
-		ft_putchar((n % 16) - 10 + 'A');
-	return (counter + 1);
+	{
+		if (ft_putchar((n % 16) - 10 + 'A') == -1)
+			return (-1);
+		*counter = *counter + 1;
+	}
+	return (*counter);
+}
+
+int	ft_hexa_upper(int n, int *counter)
+{
+	return (ft_i_hexa_upper(n, counter));
 }
