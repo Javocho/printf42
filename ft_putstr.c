edd07557364edd9da76180ac5d045b32894037cb
Francisco Javier Costa Ferrer-Vidal <fcosta-f@car11s1.42barcelona.com>
2023-07-05 15:42:48 +0200

    putstr protection for write

--- ft_putstr.c
+++ ft_putstr.c
@@ -3,26 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fcosta-f <fcosta-f@student.42barcelona.    +#+  +:+       +#+        */
+/*   By: fcosta-f <fcosta-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 19:59:46 by fcosta-f          #+#    #+#             */
-/*   Updated: 2023/07/03 20:00:51 by fcosta-f         ###   ########.fr       */
+/*   Updated: 2023/07/05 12:46:52 by fcosta-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	ft_str(char *s)
+int	ft_str(char *s, int *counter)
 {
-	int	n;
-
-	n = 0;
+	*counter = 0;
 	while (s)
 	{
 		if (write(1, s, 1) == -1)
 			return (-1);
-		n++;
+		*counter = *counter + 1;
 		s++;
 	}
-	return (n);
+	return (*counter);
 }
