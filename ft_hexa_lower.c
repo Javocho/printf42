ab4dd58ed6a968d3d0118347e52d1aa7832d2acd
gevuit <jav8yt@gmail.com>
2023-07-05 12:39:17 +0200

    ft_hexa_lower

--- ft_hexa_lower.c
+++ ft_hexa_lower.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_hexa_lower.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fcosta-f <fcosta-f@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fcosta-f <fcosta-f@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 21:19:39 by fcosta-f          #+#    #+#             */
-/*   Updated: 2023/07/05 12:21:04 by fcosta-f         ###   ########.fr       */
+/*   Updated: 2023/07/05 12:35:58 by fcosta-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,34 @@
 int	ft_i_hexa_lower(int n, int *counter)
 {
 	if (n > 15)
-		ft_i_hexa_lower(n / 16, &counter);
+	{
+		if (ft_i_hexa_lower(n / 16, counter) == -1)
+			return (-1);
+	}
 	if ((n % 16) < 10)
+	{	
 		if (ft_putchar((n % 16) + '0') == -1)
 			return (-1);
-		else
-			++(*counter);
+		*counter = *counter  + 1;
+	}
 	else
 	{
 		if (ft_putchar((n % 16) - 10 + 'a') == -1)
 			return (-1);
-		else
-			++(*counter);
+		*counter = *counter  + 1;
 	}
 	return (*counter);
 }
 
 int	ft_hexa_lower(int n, int *counter)
 {
-	return (ft_i_hexa_lower(n, &counter));
+	return (ft_i_hexa_lower(n, counter));
 }
 
 int main()
 {
     int counter = 0;
-    printf("%d", ft_hexa_lower(542, &counter));
+    printf("%d", ft_hexa_lower(255, &counter));
 
     return 0;
 }
--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: fcosta-f <fcosta-f@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 18:20:43 by fcosta-f          #+#    #+#             */
-/*   Updated: 2023/07/04 21:20:04 by fcosta-f         ###   ########.fr       */
+/*   Updated: 2023/07/05 12:35:32 by fcosta-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 # include <stdarg.h>
 # include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
 
 int	ft_printf(char const *polla, ...);
 int	ft_putchar(char c);
@@ -22,7 +24,7 @@ int	ft_types(char c, va_list ano);
 int	putstr(char *s);
 int	ft_putnbr(int nb);
 int	ft_hexa_upper(int n);
-int	ft_hexa_lower(int n);
+int	ft_hexa_lower(int n, int *counter);
 
 
 #endif
