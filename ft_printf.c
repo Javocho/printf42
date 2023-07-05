c28325f7f088253fad9d02f2f27471197765356e
Francisco Javier Costa Ferrer-Vidal <fcosta-f@car13s4.42barcelona.com>
2023-07-05 18:08:26 +0200

    c works

--- ft_printf.c
+++ ft_printf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fcosta-f <fcosta-f@student.42barcelona.    +#+  +:+       +#+        */
+/*   By: fcosta-f <fcosta-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 18:20:47 by fcosta-f          #+#    #+#             */
-/*   Updated: 2023/07/05 17:30:39 by fcosta-f         ###   ########.fr       */
+/*   Updated: 2023/07/05 18:08:13 by fcosta-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,10 @@ int	ft_printf(char const *polla, ...)
 int	ft_types(char c, va_list ano, int *count)
 {
 	if (c == 'c')
+	{
+		(*count)++;
 		return (ft_putchar(va_arg(ano, int)));
+	}
 	else if (c == 's')
 		return (ft_putstr(va_arg(ano, char *), count));
 	else if (c == 'd' || c == 'i' || c == 'u')
@@ -61,8 +64,16 @@ int	ft_types(char c, va_list ano, int *count)
 		return (ft_putchar('%'));
 	return (-1);
 }
-
-int main()
+/*int main()
 {
-	printf("%d", ft_printf("hola%c\n", '0'));
+	int a = 0;
+	int b = 0;
+
+	a = ft_printf("%c",'c');
+	printf("\n");
+	b = printf("%c", 'c');
+
+	printf("\nmi printf %d",a);
+	printf("\nsu printf %d",b);
 }
+*/
