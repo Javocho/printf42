6a34b42fc7de70a83f8d07a713efd2a06ec2f6cc
Francisco Javier Costa Ferrer-Vidal <fcosta-f@car11s1.42barcelona.com>
2023-07-05 16:07:59 +0200

    print_pointer fixed to protect writes with the same structure

--- ft_print_pointer.c
+++ ft_print_pointer.c
@@ -3,22 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_pointer.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fcosta-f <fcosta-f@student.42barcelona.    +#+  +:+       +#+        */
+/*   By: fcosta-f <fcosta-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 21:23:27 by fcosta-f          #+#    #+#             */
-/*   Updated: 2023/07/04 21:26:28 by fcosta-f         ###   ########.fr       */
+/*   Updated: 2023/07/05 16:07:38 by fcosta-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	ft_print_pointer(int n)
+int	ft_print_pointer(int n, int *count)
 {
-	int	count;
-
 	if (write(1, "0x", 2) == -1)
+	{
 		return (-1);
-	count += 2;
-	count += ft_hexa_lower(n);
+		*count += 2;
+	}
+	if (ft_hexa_lower(n, count) == -1)
+		return (-1);
 	return (count);
 }
